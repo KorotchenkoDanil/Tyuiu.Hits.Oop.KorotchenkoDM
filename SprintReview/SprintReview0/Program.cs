4d776fb653c34d1523f1ca14b78da0ab26fea593
Danil Korotchenko <danil.korotchenko.2005@gmail.ru>
2024-10-26 08:25:55 +0500

    SprintReview1,2

---
+++ SprintReview/SprintReview0/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintReview0
+{
+    class Plant
+    {
+        public string name;
+        string type;
+        float height;
+        public virtual void Blossom()
+        {
+            Console.WriteLine("Растение цветет");
+        }
+        public void Growing(float growth)
+        {
+            height += growth;
+        }
+    }
+
+    class Flower : Plant
+    {
+        string color;
+        int countOfPetal;
+        public override void Blossom()
+        {
+            Console.WriteLine($"Цветок {name} распускается и радует глаз!");
+        }
+        public void FadeAway()
+        {
+            Console.WriteLine("Цветок начинает увядать");
+        }
+    }
+
+    class Tree : Plant
+    {
+        int age;
+        string typeOfLeaves;
+        public override void Blossom()
+        {
+            Console.WriteLine($"Дерево {name} покрывается новыми листьями!");
+        }
+        public void Grow(int years)
+        {
+            age += years;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flower flower = new Flower();
+            Tree tree = new Tree();
+            flower.name = "роза";
+            tree.name = "дуб";
+            flower.Blossom();
+            tree.Blossom();
+            flower.FadeAway();
+            tree.Grow(5);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SprintReview/SprintReview1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintReview1
+{
+    class Product
+    {
+        public string name { get; set; }
+        private decimal price { get; set; }
+        private int quantity { get; set; }
+        protected string category { get; set; }
+        internal string description { get; set; }
+        public Product(string Name, decimal Price, int Quantity, string Category, string Description)
+        {
+            name = Name;
+            price = Price;
+            quantity = Quantity;
+            category = Category;
+            description = Description;
+        }
+        public decimal GetTotalPrice()
+        {
+            return (quantity * price);
+        }
+        public void UpdateQuantity(int amount)
+        {
+            quantity = amount;
+        }
+        public string GetProductInfo()
+        {
+            return ($"name: {name}, price: {price}, quantity: {quantity}, category: {category}, description: {description}");
+        }
+    }
+
+    class Seller
+    {
+        public string name { get; set; }
+        private string employeeld { get; set; }
+        protected decimal salary { get; set; }
+        internal string contactInfo { get; set; }
+        public Seller(string Name, string Employeeld, decimal Salary, string ContactInfo)
+        {
+            name = Name;
+            employeeld = Employeeld;
+            salary = Salary;
+            contactInfo = ContactInfo;
+        }
+
+        public void AddProduct(Product product)
+        {
+            Console.WriteLine($"product {product.name} added to {name}");
+        }
+        public void SellProduct(Product product, int quantity)
+        {
+            product.UpdateQuantity(quantity);
+        }
+        public string GetSellerInfo()
+        {
+            return ($"name: {name}, employeeld: {employeeld}, salary: {salary}, contact info: {contactInfo}");
+        }
+    }
+
+    class Store
+    {
+        private string storeName { get; set; }
+        private string location { get; set; }
+        public string storeHours { get; set; }
+        public Store(string StoreName, string Location, string StoreHours)
+        {
+            storeName = StoreName;
+            location = Location;
+            storeHours = StoreHours;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Console.WriteLine($"seller {seller.name} added to {storeName}");
+        }
+        public void ListProducts(Product product)
+        {
+            Console.WriteLine($"list of products: {product.name}");
+        }
+        public string GetStoreInfo()
+        {
+            return ($"store name: {storeName}, location: {location}, store hours: {storeHours}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Milk", 10, 25, "Drinks", "description");
+            Console.WriteLine("total price = " + product.GetTotalPrice());
+            Console.WriteLine(product.GetProductInfo());
+            Seller seller = new Seller("Andrey", "Seller", 5000, "contact info");
+            seller.AddProduct(product);
+            Console.WriteLine(seller.GetSellerInfo());
+            Store store = new Store("Ashan", "Mendeleeva 1", "8");
+            store.AddSeller(seller);
+            store.ListProducts(product);
+            Console.WriteLine(store.GetStoreInfo());
+            Console.ReadKey();
+        }
+    }
+}
